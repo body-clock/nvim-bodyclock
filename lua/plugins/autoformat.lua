680ea1aecca440b56bdcc1777682c7e2965b7847
Patrick Perkins <lilblizzard97@gmail.com>
2024-05-20 22:13:20 -0400

    disable copilot, format ruby with rubocop

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -25,14 +25,12 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      ruby = { 'ruby_lsp' },
-      javascript = { 'prettierd' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
+      ruby = { 'rubocop' },
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
+      javascript = { 'prettierd', 'prettier' },
+      -- Conform can also run multiple formatters sequentially
+      -- python = { "isort", "black" },
     },
   },
 }
